e14a72418068582eb2b125880d9e5719293eeb19
oliverxhu <oliver.hu@hellofresh.com.au>
2017-06-16 11:03:58 +1000

    date lookup fix

--- Listings.py
+++ Listings.py
@@ -2,6 +2,7 @@ import time
 import json
 import pandas as pd
 import ETLTools
+import numpy as np
 from Email_Tools import EmailConnection
 
 
@@ -47,32 +48,35 @@ class Listings(ETLTools.ETLTools):
     def run_all_etl(self):
         opening = time.time()
         tries = 0
-        emc = EmailConnection(aws_id='AKIAJUKW4BKB6I2O3JOA', aws_secret='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')
+        #emc = EmailConnection(aws_id='AKIAJUKW4BKB6I2O3JOA', aws_secret='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')
         while tries < 2:
+            print('initialising transaction')
             self.con_data.create_transaction()
-            self.update_agency()
             self.update_rental_listings()
+            self.update_agency()
             self.update_agent_rental()
             self.update_rental_photo()
+            #if (self.rental_listings_flag) == 1:
             if (self.agency_flag + self.rental_listings_flag + self.agent_rental_flag + self.rental_photo_flag) == 4:
                 self.con_data.commit_transaction()
                 self.con_data.close_connection()
-                emc.send_email(from_address='housedatawebsite@gmail.com',
-                               to_address_list=['housedatawebsite@gmail.com'],
-                               subject='Success! %s' % str(self.datenow()), body='')
+                # emc.send_email(from_address='housedatawebsite@gmail.com',
+                #                to_address_list=['housedatawebsite@gmail.com'],
+                #                subject='Success! %s' % str(self.datenow()), body='')
                 print('total time: %s' % (time.time() - opening))
                 exit()
             else:
                 self.con_data.rollback_transaction()
                 tries += 1
+        print('closing connection')
         self.con_data.close_connection()
         error_list = []
         for e in enumerate([self.agent_rental_error, self.rental_listings_error,
                             self.agency_error, self.rental_photo_error]):
             if e:
                 error_list.append(e)
-        emc.send_email(from_address='housedatawebsite@gmail.com', to_address_list=['housedatawebsite@gmail.com'],
-                       subject='Error!', body='errors: \n' + str(error_list))
+        # emc.send_email(from_address='housedatawebsite@gmail.com', to_address_list=['housedatawebsite@gmail.com'],
+        #                subject='Error!', body='errors: \n' + str(error_list))
 
     """Generic Functions"""
     def _control(self, table):
@@ -142,8 +146,83 @@ class Listings(ETLTools.ETLTools):
             df[cols] = df[cols].astype('datetime64[ns]')
         return df
 
+    def update_rental_listings(self, table='rental_listings'):
+        """Completes jobs for rental_listings"""
+        """Issues
+            Update Existing records is not joining properly to the table
+        """
+        try:
+            print('UPDATE_RENTAL_LISTINGS')
+
+            source_cols = self._control_source_columns(table=table)
+            destination_cols = self._control_destination_columns(table, key=False, meta=False, mapping=True)
+            date_cols = self._control_date_cols(table)
+
+            staging_listings_df = self.generator_to_df(self.con_staging.query('SELECT %s FROM tm_rental_listings_hourly'
+                                                       % ', '.join(source_cols)), columns=destination_cols)
+            staging_listings_df = staging_listings_df.assign(is_current=1, valid_from=self.datenow(),
+                                                             valid_to=self.dateend(), row_insert_date=self.datenow())
+            staging_listings_df = self.convert_ms_dates(staging_listings_df, table)
+
+            # 1) Add new records
+            self.con_data.add_new_records(df=staging_listings_df, df_lookup_column_list=['id_listing_tm'],
+                                          table=table, table_lookup_column_list=['id_listing_tm'])
+
+            staging_listings_df['start_date'] = staging_listings_df['start_date'].apply(lambda x: np.datetime64(x))
+            staging_listings_df['end_date'] = staging_listings_df['end_date'].apply(lambda x: np.datetime64(x))
+
+            # 3) update existing records
+            print('***update existing records***')
+
+            update_df = self.con_data.table_lookup(df=staging_listings_df,
+                                                   df_lookup_column_list=destination_cols,  # destination_cols,
+                                                   table=table,
+                                                   table_lookup_column_list=destination_cols,  # destination_cols,
+                                                   table_return_column_list=[],
+                                                   wheresql="WHERE is_current = 1",
+                                                   convert_datetime_cols=date_cols)
+
+            update_df = update_df[update_df['_merge'] == 'left_only']
+
+            print('***len updated: %s *** ' % len(update_df))
+
+            if not len(update_df) == 0:
+                # update old rows
+                for ind, row in update_df.iterrows():
+                    self.con_data.query("""UPDATE %s
+                                           SET is_current = 0, valid_to = '%s' 
+                                           WHERE id_listing_tm = '%s'
+                                           AND is_current = 1
+                                           """ % (table, self.datenow(), row['id_listing_tm']))
+                # write new rows
+                update_df = update_df[staging_listings_df.columns]
+
+                self.con_data.append_df_to_table(df=update_df, table=table)
+
+            # 2) deactivate rows that aren't active anymore (ie. in datalayer but not staging)
+            # this is changing everything to old
+            print('***deactivating_old_listings***')
+            old_record_list = self.con_data.find_old_records(df=staging_listings_df,
+                                                             df_lookup_column_list=['id_listing_tm'],
+                                                             table=table,
+                                                             table_lookup_column_list=['id_listing_tm'],
+                                                             table_pk='id_rental_listing',
+                                                             convert_datetime_cols=date_cols)
+            print('number of old records: %s ' % len(old_record_list))
+            if len(old_record_list) != 0:
+                print('deactivating %d records' % len(old_record_list))
+                self.con_data.query("UPDATE rental_listings SET is_current=0, valid_to='%s' WHERE id_rental_listing IN(%s)"
+                                    % (self.dateend(), ', '.join(list(old_record_list['id_rental_listing']
+                                                                      .apply(lambda x: str(x))))))
+
+            self.rental_listings_flag = 1
+        except Exception as e:
+            self.rental_listings_error = e
+
     """Table Update Functions"""
     def update_agency(self, table='agency'):
+
+        print('\nUPDATE_AGENCY')
         """Updates the agency table"""
         try:
             source_agency_cols = self._control_source_columns(table, return_as_str=True)
@@ -166,71 +245,10 @@ class Listings(ETLTools.ETLTools):
         except Exception as e:
             self.agency_error = e
 
-    def update_rental_listings(self, table='rental_listings'):
-        """Completes jobs for rental_listings"""
-        """Issues
-            Update Existing records is not joining properly to the table
-        """
-        # try:
-        source_cols = self._control_source_columns(table=table)
-        destination_cols = self._control_destination_columns(table, key=False, meta=False,
-                                                             mapping=True)
-        date_cols = self._control_date_cols(table)
-        staging_listings_df = self.generator_to_df(self.con_staging.query('SELECT %s FROM tm_rental_listings_hourly'
-                                                   % ', '.join(source_cols)), columns=destination_cols)
-        staging_listings_df = staging_listings_df.assign(is_current=1, valid_from=self.datenow(),
-                                                         valid_to=self.dateend(), row_insert_date=self.datenow())
-        staging_listings_df = self.convert_ms_dates(staging_listings_df, table)
-
-        # 1) Add new records
-        self.con_data.add_new_records(df=staging_listings_df, df_lookup_column_list=['id_listing_tm'],
-                                      table=table, table_lookup_column_list=['id_listing_tm'])
-
-        # staging_listings_df['start_date'] = staging_listings_df['start_date'].astype('datetime64[s, UTC]')
-        # staging_listings_df['end_date'] = staging_listings_df['end_date'].astype('datetime64[s, UTC]')
-
-        # 2) deactivate columns that aren't active anymore (ie. in datalayer but not staging)
-        # this is changing everything to old
-        old_record_list = self.con_data.find_old_records(df=staging_listings_df,
-                                                         df_lookup_column_list=['id_listing_tm'],
-                                                         table=table,
-                                                         table_lookup_column_list=['id_listing_tm'],
-                                                         table_pk='id_rental_listing',
-                                                         convert_datetime_cols=date_cols)
-
-        if len(old_record_list) != 0:
-            self.con_data.query("UPDATE rental_listings SET is_current=0, valid_to='%s' WHERE id_rental_listing IN(%s)"
-                                % (self.dateend(), ', '.join(old_record_list)))
-
-        # 3) update existing records
-        update_df = self.con_data.table_lookup(df=staging_listings_df,
-                                               df_lookup_column_list=destination_cols,  # destination_cols,
-                                               table=table,
-                                               table_lookup_column_list=destination_cols,  # destination_cols,
-                                               table_return_column_list=[],
-                                               wheresql="WHERE is_current = 1",
-                                               convert_datetime_cols=date_cols)
-
-        update_df = update_df[update_df['_merge'] == 'left_only']
-
-        if not len(update_df) == 0:
-            # update old rows
-            for ind, row in update_df.iterrows():
-                self.con_data.query("""UPDATE %s
-                                       SET is_current = 0, valid_to = '%s' 
-                                       WHERE id_listing_tm = '%s'
-                                       AND is_current = 1
-                                       """ % (table, self.datenow(), row['id_listing_tm']))
-            # write new rows. What is this doing?
-            update_df = update_df[staging_listings_df.columns]
-
-            self.con_data.append_df_to_table(df=update_df, table=table)
-
-        self.rental_listings_flag = 1
-        # except Exception as e:
-        #     self.rental_listings_error = e
-
     def update_agent_rental(self, table='rental_agent'):
+
+        print('\nUPDATE_RENTAL_AGENT')
+
         """Updates the agent_rental table"""
         # need to change the update strategy here. It doesn't add rows with 2 agents
         try:
@@ -267,6 +285,8 @@ class Listings(ETLTools.ETLTools):
             self.agent_rental_error = e
 
     def update_rental_photo(self, table='rental_photo'):
+
+        print('UPDATE RENTAL PHOTO')
         """Updates the rental_photos table"""
         # get source cols as list, and compare with long table of photo keyshe wont
         try:
